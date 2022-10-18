12e7f5d4f671b7bce7934c1b6c1b107da3c784d1
Thomas Cavalli <thomas.cavalli@gmail.com>
2022-10-18 11:01:11 -0300

    Add authentication handler controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'auth/sign_in'
+  get 'auth/sign_out'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
---
+++ test/controllers/auth_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class AuthControllerTest < ActionDispatch::IntegrationTest
+  test "should get sign_in" do
+    get auth_sign_in_url
+    assert_response :success
+  end
+
+  test "should get sign_out" do
+    get auth_sign_out_url
+    assert_response :success
+  end
+
+end
